3af5ae4bb80b3ebd8c6693637bfa91917497360b
Alexandr Mykovnya <frion322@gmail.com>
2021-08-24 23:20:13 +0300

    create hotfix

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ export default function Header  ({toDo, done})  {
     return (
       <div className="app-header d-flex">
         <h1>Todo List</h1>
-        <h2>{toDo} more to do, {done} done</h2>
+        <h2>{toDo} more to dos, {done} done</h2>
       </div>
     );
   };
